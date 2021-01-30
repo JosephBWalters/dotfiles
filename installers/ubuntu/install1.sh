e40b6e1bc1b04af93e070296c9ed58dfee46617e
Joseph Walters <joseph.walters@hey.com>
2021-01-30 18:01:44 -0500

    feat(linux): add linux installers

---
+++ installers/ubuntu/install1.sh
@@ -0,0 +1,33 @@
+# needs to be run in sudo!
+apt install build-essential
+apt install zlib1g-dev
+apt install htop
+apt install neofetch
+apt install fzf
+apt install neovim
+apt install curl
+apt install rbenv
+apt install ripgrep
+apt install tldr
+apt install yarnpkg
+apt install autojump
+apt install python3-venv
+apt install stow
+sudo apt install silversearcher-ag
+apt install thefuck
+apt install postgresql
+apt install zsh
+
+# Install Chrome Browser
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Install VSCode
+sudo snap install --classic code # or code-insiders
+
+# Installs nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+# Installs oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# after zsh loads, restart shell and run install2.sh
---
+++ installers/ubuntu/install3.sh
@@ -0,0 +1,12 @@
+# TODO: prompt node version
+echo 'nvm setup!'
+nvm install --lts
+
+echo 'rbenv setup!'
+rbenv init
+# TODO prompt ruby version
+rbenv install 2.4.1
+rbenv global 2.4.1
+gem update 
+gem install bundler
+rbenv rehash
